46c4da04693e1cc694e56a854ab0511400c0d28d
is.a.clam <isaaccylam@gmail.com>
2021-06-15 23:55:53 +0800

    pls work

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / 'public'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from graph import views
+from .views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('graph.urls')),
