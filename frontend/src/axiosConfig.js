af726bd5b8bd74ca5df08e0bfe76400699006b1b
is.a.clam <isaaccylam@gmail.com>
2021-06-21 11:27:46 +0800

    asdf

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,13 @@
 import axios from "axios"
 
+const backendURIs = {
+  development: 'http://localhost:8000/',
+  production: 'http://stock-app-cs-ia.herokuapp.com/',
+}
+
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = backendURIs[process.env.NODE_ENV]
 axios.defaults.withCredentials = true
+
 export default axios
